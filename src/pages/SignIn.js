33c38ca4d68032aad2594c31c40c9e2c2d7673f7
Sania Khushbakht Jamil <saniak.jamil@gmail.com>
2019-03-16 23:23:08 +0500

    tried prettier configuration on SignIn file

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { withStyles, Grid } from '@material-ui/core/styles';
-
+import { withStyles, Grid } from "@material-ui/core/styles";
 
 const SignIn = () => {
-    return(
-        <h1>HAHA</h1>
-    );
+  return <h1>HAHA</h1>;
 };
 
 export default SignIn;
